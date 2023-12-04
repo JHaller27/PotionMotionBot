0e25ef15b0c89f6893c43b2ef4575a14b2c93568
James <jameshaller27@gmail.com>
2023-12-03 22:46:25 -0800

    Show image split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import ImageGrab, Image
 import pygame
+from pygame.event import Event
 
 from dataclasses import dataclass
 from typing import Self, Iterator
@@ -23,9 +24,7 @@ class GuideParams:
 		cell_size = (self.size[0] // GRID_SIZE[0], self.size[1] // GRID_SIZE[1])
 		for x in range(GRID_SIZE[0]):
 			for y in range(GRID_SIZE[1]):
-				rect = (cell_size[0] * x + self.top_left[0], cell_size[1] * y + self.top_left[1])
-				rect = (rect[0], rect[1], cell_size[0], cell_size[1])
-				yield rect
+				yield pygame.Rect(cell_size[0] * x + self.top_left[0], cell_size[1] * y + self.top_left[1], cell_size[0], cell_size[1])
 
 
 def get_grid_guide(size: tuple[int, int], guide_params: GuideParams) -> pygame.Surface:
@@ -42,18 +41,19 @@ class DataContext:
 	window: pygame.Surface
 	background_surface: pygame.Surface
 	guide_params: GuideParams
+	pil_image: Image
 
 
 class State:
 	def __init__(self, ctx: DataContext) -> None:
 		self._ctx: DataContext = ctx
 
-	def handle(self, events: list[pygame.event.Event]) -> Self | None:
+	def handle(self, events: list[Event]) -> Self | None:
 		raise NotImplementedError
 
 
 class SelectTopLeftState(State):
-	def handle(self, events: list[pygame.event.Event]) -> Self | None:
+	def handle(self, events: list[Event]) -> Self | None:
 		self._ctx.guide_params = GuideParams(pygame.mouse.get_pos(), (100, 100), 'red')
 		grid_surface = get_grid_guide(self._ctx.window.get_size(), self._ctx.guide_params)
 
@@ -68,7 +68,7 @@ class SelectTopLeftState(State):
 
 
 class SetSizeState(State):
-	def handle(self, events: list[pygame.event.Event]) -> Self | None:
+	def handle(self, events: list[Event]) -> Self | None:
 		mouse_pos = pygame.mouse.get_pos()
 		size = (mouse_pos[0] - self._ctx.guide_params.top_left[0], mouse_pos[1] - self._ctx.guide_params.top_left[1])
 
@@ -89,15 +89,28 @@ class SetSizeState(State):
 
 
 class WaitState(State):
-	def handle(self, events: list[pygame.event.Event]) -> Self | None:
+	def handle(self, events: list[Event]) -> Self | None:
 		grid_surface = get_grid_guide(self._ctx.window.get_size(), self._ctx.guide_params)
 
 		self._ctx.window.blit(self._ctx.background_surface, self._ctx.background_surface.get_rect())
 		self._ctx.window.blit(grid_surface, grid_surface.get_rect())
 
 		for event in events:
-			if event.type == pygame.MOUSEBUTTONUP and event.button == 3:
+			if event.type == pygame.KEYUP and event.key == pygame.K_RETURN:
+				return ShowImageSplitState(self._ctx)
+			elif event.type == pygame.MOUSEBUTTONUP and event.button == 3:
 				return SetSizeState(self._ctx)
+		return self
+
+
+class ShowImageSplitState(State):
+	def handle(self, events: list[Event]) -> Self | None:
+		for cell_rect in self._ctx.guide_params.get_cell_rects():
+			subimg = self._ctx.pil_image.crop((cell_rect.left, cell_rect.top, cell_rect.right, cell_rect.bottom))
+			sub_surface = pil_image_to_surface(subimg)
+
+			self._ctx.window.blit(sub_surface, cell_rect)
+
 		return self
 
 
@@ -111,7 +124,7 @@ def main():
 	screencap_surface = pil_image_to_surface(pil_image)
 
 	clock = pygame.time.Clock()
-	current_state = SelectTopLeftState(DataContext(window, screencap_surface, None))
+	current_state = SelectTopLeftState(DataContext(window, screencap_surface, None, pil_image))
 
 	while current_state is not None:
 		clock.tick(60)
