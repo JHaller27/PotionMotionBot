08609561110d8394ce7b3388c7d28a3f0119e671
James <jameshaller27@gmail.com>
2023-12-06 12:48:07 -0800

    Update config to use bash-style guards

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import pygame
 
 
 ENABLE_MOUSE: bool = True
-PROMPT_AFTER_DRAG: int | None = None  #pygame.K_RETURN
-PROMPT_AFTER_SHOW_DRAG: int | None = None  #pygame.K_RETURN
-PROMPT_TO_SCREENCAP: str | None = None  #'x'
-ESCAPE_FROM_PLAY_KEY: str | None = 'ESCAPE'
+PROMPT_AFTER_DRAG: int | None = None and pygame.K_RETURN
+PROMPT_AFTER_SHOW_DRAG: int | None = None and pygame.K_RETURN
+PROMPT_TO_SCREENCAP: str | None = None and 'x'
+ESCAPE_FROM_PLAY_KEY: str | None = None or 'ESCAPE'
