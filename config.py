203415dda15a44f59a7f2b658f75aac43db98516
James <jameshaller27@gmail.com>
2023-12-09 20:19:01 -0800

    Add live config reloading

--- config.py
+++ config.py
@@ -5,12 +5,21 @@ from pathlib import Path
 DATA_PATH = Path() / 'data' / 'config.toml'
 
 
-with DATA_PATH.open('rb') as fp:
-	config = tomllib.load(fp)
+def load_config():
+	global config
+
+	with DATA_PATH.open('rb') as fp:
+		config = tomllib.load(fp)
 
 
 def get_config(*parts):
+	if config['General']['liveConfigs']:
+		load_config()
+
 	curr = config
 	for part in parts:
 		curr = curr[part]
 	return curr
+
+
+load_config()
